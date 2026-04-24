a6ae619ec66e474778117957ca445689d4d6efb3
cooperstephen-jpg <cooper.stephen@gmail.com>
2026-04-23 23:29:57 -0400

    Update scan.js

--- scan.js
+++ scan.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const fs = require('fs');
 
 const SERPAPI_KEY = process.env.SERPAPI_KEY;
